2c657c6127dd59c5decf360c809c7099a7fa8266
drew <drewmcneely@email.arizona.edu>
2019-12-14 20:32:45 -0600

    Updated dock.sh to use new monitors

--- bin/dock.sh
+++ bin/dock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xrandr --output LVDS1 --auto --primary --rotate normal --pos 0x540 --output VGA1 --auto --rotate left --pos 1600x0
+xrandr --output LVDS1 --off --output VGA1 --auto --primary --rotate normal --pos 0x510 --output DP3 --auto --rotate left --pos 1920x0
 feh --bg-fill '/home/drew/etc/display/backgrounds/horizontal/j80s.jpg' '/home/drew/etc/display/backgrounds/vertical/cactus.jpg'
